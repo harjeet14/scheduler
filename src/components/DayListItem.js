3677a3c45bca2b781cbbc5cefcf6969089ab8069
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-11-09 13:50:45 -0500

    add daylist to show days with remaining spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,17 +1,28 @@
 import React from "react";
 import classNames from "classnames";
 import "./DayListItem.scss";
+
 export default function DayListItem(props) {
 
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots
   });
+  const formatSpots = function () {
+    let spotRemaining;
+    if (props.spots === 0) {
+      spotRemaining = "no spots remaining"
+    }
+    else if (props.spots > 0) {
+      spotRemaining = `${props.spots} ${props.spots === 1 ? "spot" : "spots"} remaining`
+    }
 
+    return spotRemaining;
+  }
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
