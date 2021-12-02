8ad906d4fc004e17c1c82319380fca988adb793a
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-12-02 14:13:35 -0500

    add proptypes library for test

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,9 +2,11 @@ import React from "react";
 import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 import "components/InterviewerListItem.scss";
+import PropTypes from 'prop-types';
 
 
 export default function InterviewerList(props) {
+
   const interviewers = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
@@ -16,6 +18,7 @@ export default function InterviewerList(props) {
       />
     )
   });
+
   return <section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
     <ul className="interviewers__list">
@@ -23,4 +26,8 @@ export default function InterviewerList(props) {
     </ul>
   </section>
 }
+InterviewerList.propTypes = {
+  interviewers: PropTypes.func.isRequired
+};
+
 
